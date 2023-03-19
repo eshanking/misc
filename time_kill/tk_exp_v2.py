2cf6274bbadff7bc2c8211149c269082eb575532
King <kinge2@lri-108705.cc.ad.cchs.net>
2023-03-19 18:08:22 -0400

    more work on tk

--- time_kill/tk_exp_v2.py
+++ time_kill/tk_exp_v2.py
@@ -25,6 +25,83 @@ dc = [c*mic for c in dc]
 
 filehandler = open('normalizer.p', 'rb') 
 norm = pickle.load(filehandler)
+
+#%% Stitcher function to stich multiple scans together
+
+def stitcher(exp_folder,background=None,bg_row='H'):
+
+    plate_list = []
+    od_data_list = []
+
+    exp_folder = 'tk_02282023'
+
+    exp_files = os.listdir(exp_folder)
+
+    exp_files = [f for f in exp_files if f[-5:] == '.xlsx']
+
+    cur_col = 1
+
+    for ef in exp_files:
+        path_t = os.getcwd() + os.sep + exp_folder + os.sep + ef
+        p = AutoRate.Plate(path_t)
+
+        od_data = p.parse_data_file(path_t,data_start='OD600')
+        od_data_list.append(od_data)
+        
+        p.data = p.data.drop(p.data.index[0:2])
+
+
+        for col in range(1,cur_col+1):
+            
+            bg_key = bg_row + str(col)
+            bg_data = p.data[bg_key]
+
+            for row in row_list[0:-1]:
+
+                key = row + str(col)
+                ts = p.data[key]
+
+                bg_data[bg_data==0] = 1
+
+                ts[ts=='OVER'] = np.nan
+                if background == 'subtract':
+                    ts = ts - bg_data
+                elif background == 'divide':
+                    ts = np.divide(ts,bg_data)
+
+                p.data[key] = ts
+        dt = p.get_start_time()
+        start_time = 60*((60*dt.hour) + dt.minute) + dt.second
+        sample_times.append(start_time/60)
+        # print(start_time)
+        p.data['Time [s]'] = p.data['Time [s]'] + start_time
+
+        od_data['Time [s]'] = od_data['Time [s]'] + start_time
+
+        plate_list.append(p)
+        cur_col+=1
+
+    data = plate_list[0].data
+    od_data = od_data_list[0]
+
+    for od_d in od_data_list[1:]:
+        od_data = pd.concat((od_data,od_d))
+
+    for p in plate_list[1:]:
+        data = pd.concat((data,p.data))
+
+    data = data.sort_values(by='Time [s]')
+
+    data['Time [s]'] = data['Time [s]'] - np.min(data['Time [s]'])
+
+    od_data['Time [s]'] = od_data['Time [s]'] - np.min(od_data['Time [s]'])
+
+    sample_times = np.array(sample_times) - np.min(sample_times)
+    data['est. sample times']
+                    
+
+
+
 #%%
 def rolling_average(x,N):
     
